73311ed43f8d9422c5459da7198a0fa8cfb80211
Parth <parthdharmale008@gmail.com>
2024-04-23 10:09:00 +0530

    Read Training Data from CSV

--- src/main/java/App2.java
+++ src/main/java/App2.java
@@ -1,277 +1,47 @@
 
-// // import com.opencsv.CSVReader;
-
-// import java.util.ArrayList;
-// import java.util.List;
-// import java.util.Arrays;
-// import java.util.Random;
-// import java.util.Scanner;
-// import java.lang.Math;
-// import javax.swing.JOptionPane;
-
-// public class App2 {
-//     public static void main(String[] args) {
-//         App2 app = new App2();
-//         Scanner scanner = new Scanner(System.in);
-
-//         System.out.print("Enter the number of layers: ");
-//         int numLayers = scanner.nextInt();
-//         System.out.print("Enter the number of epochs you want to train the model on: ");
-//         int epochs = scanner.nextInt();
-        
-//         scanner.close();
-//         app.trainAndPredict(numLayers, epochs);
-//     }
-
-//     public void trainAndPredict(int numLayers, int epochs) {
-
-//         // System.out.println(numLayers);
-//         List<List<Integer>> data = new ArrayList<List<Integer>>();
-//         data.add(Arrays.asList(115, 66));//Female
-//         data.add(Arrays.asList(175, 78));//Male
-//         data.add(Arrays.asList(205, 72));//Male
-//         data.add(Arrays.asList(120, 67));//Female
-//         data.add(Arrays.asList(120, 52));//Female
-//         data.add(Arrays.asList(185, 87));//Male
-//         data.add(Arrays.asList(199, 77));//Male
-//         data.add(Arrays.asList(105, 60));//Female
-//         data.add(Arrays.asList(150, 66));//Female
-//         data.add(Arrays.asList(160, 78));//Male
-//         data.add(Arrays.asList(189, 76));//Male
-//         data.add(Arrays.asList(142, 60));//Female
-
-//         List<Double> answers = Arrays.asList(1.0, 0.0, 0.0, 1.0, 1.0,0.0,0.0,1.0,1.0,0.0,0.0,1.0);
-
-//         Network network500 = new Network(numLayers, epochs);
-
-//         // Train the network
-//         network500.train(data, answers);
-
-//         // Network network1000 = new Network(numLayers, 1000);
-//         // network1000.train(data, answers);
-
-//         System.out.println("");
-
-//         System.out.println(String.format("  male, 167, 73: network500: %.10f ", network500.predict(170, 73)));
-//         System.out.println(String.format("female, 105, 67: network500: %.10f ", network500.predict(105, 61)));
-//         System.out.println(String.format("female, 120, 72: network500: %.10f ", network500.predict(120, 72)));
-//         System.out.println(String.format(" male, 143, 67: network500: %.10f ", network500.predict(173, 77)));
-//         System.out.println(String.format(" male, 150, 66: network500: %.10f ", network500.predict(170, 66)));
-
-//     }
-
-//     class Network {
-//         int epochs = 0; // 1000;.
-//         int numLayers = 4;
-//         Double learnFactor = null;
-//         private List<Neuron> neurons;
-
-//         public Network(int numLayers, int epochs) {
-//             this.numLayers = numLayers;
-//             this.epochs = epochs;
-//             this.neurons = generateNeurons(numLayers);
-//         }
-
-//         // Generate
-//         // Neurons-------------------------------------------------------------------------------------
-
-//         public List<Neuron> generateNeurons(int numLayers) {
-//             int numNeurons = numLayers * (numLayers + 1) / 2;
-//             List<Neuron> temp = new ArrayList<>(numNeurons);
-//             for (int i = 0; i < numNeurons; i++) {
-//                 temp.add(new Neuron());
-//             }
-//             System.out.println(numLayers);
-//             System.out.println(numNeurons);
-//             return temp;
-//         }
-
-//         public Network(int epochs, Double learnFactor) {
-//             this.epochs = epochs;
-//             this.learnFactor = learnFactor;
-//         }
-
-//         // Predict
-//         // Method-------------------------------------------------------------------------------------
-
-//         public Double predict(double input1, double input2) {
-//             int numLayers = this.numLayers;
-//             // System.out.println(numLayers);
-//             // System.out.println("We are inside predict function now");
-
-//             int n = numLayers * (numLayers + 1) / 2; // Number of neurons
-
-//             int last = n - 1; // Index of the Last neuron in array
-//             int counter = numLayers; // Index of the layer from right.
-
-//             int currNeuron = counter - 1; // Number of neurons left to process in current layer
-
-//             double[] dp = new double[n]; // DP array to store intermediate results
-//             Arrays.fill(dp, 0);
-
-//             for (int i = 0; i <= last; i++) {
-//                 // If the index of current neuron is < num of layers, it means the neuron belongs to input layer hence takes direct user input
-//                 if (i < numLayers) {
-                    
-//                     dp[i] = neurons.get(i).compute(input1, input2);
-//                 } // Else it takes input from two neurons from the previous layer
-//                 else {
-//                     dp[i] = neurons.get(i).compute(dp[i - counter], dp[i - counter + 1]);
-
-//                     currNeuron--;
-//                     if (currNeuron == 0) {
-//                         // System.out.println("We are in Layer " + (numLayers - counter + 2));
-//                         counter--;
-//                         currNeuron = counter - 1;
-//                     }
-//                 }
-//             }
-//             return dp[last];
-//         }
-
-//         // Backpropagation
-//         // ----------------------------------------------------------------------------
-//         public void backpropagation(List<List<Integer>> data, List<Double> answers, double learningRate) {
-//             for (int i = 0; i < 4; i++) {
-//                 double input1 = data.get(i).get(0);
-//                 double input2 = data.get(i).get(1);
-//                 double predictedOutput = predict(input1, input2);
-//                 double actualOutput = answers.get(i);
-//                 double error = actualOutput - predictedOutput;
-
-//                 // Update weights and biases using backpropagation algorithm
-//                 for (Neuron neuron : neurons) {
-//                     double derivative = Util.sigmoidDeriv(predictedOutput);
-//                     double delta = error * derivative;
-//                     neuron.bias += learningRate * delta;
-//                     neuron.weight1 += learningRate * delta * input1;
-//                     neuron.weight2 += learningRate * delta * input2;
-//                 }
-//             }
-//         }
-
-//         // Train
-//         // Method-------------------------------------------------------------------------------------
-
-//         public void train(List<List<Integer>> data, List<Double> answers) {
-//             int numLayers = this.numLayers;
-
-//             int n = numLayers * (numLayers + 1) / 2;
-//             System.out.println(epochs);
-//             Double bestEpochLoss = null;
-//             for (int epoch = 0; epoch < epochs; epoch++) {
-//                 // adapt neuron
-//                 Neuron epochNeuron = neurons.get(epoch % 6);
-//                 epochNeuron.mutate(this.learnFactor);
-
-//                 List<Double> predictions = new ArrayList<Double>();
-//                 for (int i = 0; i < 4; i++) {
-//                     predictions.add(i, this.predict(data.get(i).get(0), data.get(i).get(1)));
-//                 }
-//                 Double thisEpochLoss = Util.meanSquareLoss(answers, predictions);
-
-//                 if (epoch % 50 == 0)
-//                     System.out.println(String.format("Epoch: %s | bestEpochLoss: %.15f | thisEpochLoss: %.15f", epoch,
-//                             bestEpochLoss, thisEpochLoss));
-
-//                 if (bestEpochLoss == null) {
-//                     bestEpochLoss = thisEpochLoss;
-//                     epochNeuron.remember();
-//                 } else {
-//                     if (thisEpochLoss < bestEpochLoss) {
-//                         bestEpochLoss = thisEpochLoss;
-//                         epochNeuron.remember();
-//                     } else {
-//                         epochNeuron.forget();
-//                     }
-//                 }
-//             }
-//         }
-
-//     }
-
-//     class Neuron {
-//         Random random = new Random();
-  
-//         private Double oldBias = random.nextDouble(-1, 1), bias = 4.227263774752734;
-//         public Double oldWeight1 = random.nextDouble(-1, 1), weight1 = -8.188992192876949;
-//         private Double oldWeight2 = random.nextDouble(-1, 1), weight2 = -4.67817295490573;
-
-//         public String toString() {
-//             return String.format(
-//                     "oldBias: %.15f | bias: %.15f | oldWeight1: %.15f | weight1: %.15f | oldWeight2: %.15f | weight2: %.15f",
-//                     this.oldBias, this.bias, this.oldWeight1, this.weight1, this.oldWeight2, this.weight2);
-//         }
-
-//         public void mutate(Double learnFactor) {
-//             int propertyToChange = random.nextInt(0, 3);
-//             Double changeFactor = (learnFactor == null) ? random.nextDouble(-1, 1)
-//                     : (learnFactor * random.nextDouble(-1, 1));
-//             if (propertyToChange == 0) {
-//                 this.bias += changeFactor;
-//             } else if (propertyToChange == 1) {
-//                 this.weight1 += changeFactor;
-//             } else {
-//                 this.weight2 += changeFactor;
-//             }
-//             ;
-//         }
-
-//         public void forget() {
-//             bias = oldBias;
-//             weight1 = oldWeight1;
-//             weight2 = oldWeight2;
-//         }
-
-//         public void remember() {
-//             oldBias = bias;
-//             oldWeight1 = weight1;
-//             oldWeight2 = weight2;
-//         }
-
-//         public double compute(double input1, double input2) {
-//             // this.input1 = input1; this.input2 = input2;
-//             double preActivation = (this.weight1 * input1) + (this.weight2 * input2) + this.bias;
-//             double output = Util.sigmoid(preActivation);
-//             return output;
-//         }
-//     }
-
-//     class Util {
-//         public static double sigmoid(double in) {
-//             return 1 / (1 + Math.exp(-in));
-//         }
-
-//         public static double sigmoidDeriv(double in) {
-//             double sigmoid = Util.sigmoid(in);
-//             return sigmoid * (1 - in);
-//         }
-
-//         /** Assumes array args are same length */
-//         public static Double meanSquareLoss(List<Double> correctAnswers, List<Double> predictedAnswers) {
-//             double sumSquare = 0;
-//             for (int i = 0; i < predictedAnswers.size(); i++) {
-//                 double error = correctAnswers.get(i) - predictedAnswers.get(i);
-//                 sumSquare += (error * error);
-//             }
-//             return sumSquare / (correctAnswers.size());
-//         }
-//     }
-// }
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.swing.*;
 public class App2 {
     public static void main(String[] args) {
+
+        List<List<Integer>> data = new ArrayList<>();
+        List<Double> lastColumn = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream("testdata.csv"), StandardCharsets.UTF_8))) {
+
+            String line;
+            br.readLine(); // Skip the header line
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(",");
+                List<Integer> row = new ArrayList<>();
+                row.add(Integer.valueOf(values[0]));
+                row.add(Integer.valueOf(values[1]));
+                data.add(row);
+                lastColumn.add(Double.parseDouble(values[2]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+
         SwingUtilities.invokeLater(() -> {
-            new App2().createAndShowGUI();
+            new App2().createAndShowGUI(data, lastColumn);
         });
     }
 
-    private void createAndShowGUI() {
+    private void createAndShowGUI(List<List<Integer>> data, List<Double> lastColumn) {
         JFrame frame = new JFrame("Gender Prediction");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -303,7 +73,7 @@ public class App2 {
             int epochs = Integer.parseInt(epochsField.getText());
             int newHeight = Integer.parseInt(heightField.getText());
             int newWeight = Integer.parseInt(weightField.getText());
-            trainAndPredict(numLayers, epochs, newHeight, newWeight);
+            trainAndPredict(numLayers, epochs, newHeight, newWeight, data, lastColumn);
         });
 
         frame.getContentPane().add(panel);
@@ -311,27 +81,12 @@ public class App2 {
         frame.setVisible(true);
     }
 
-    public void trainAndPredict(int numLayers, int epochs, int newHeight, int newWeight) {
-        List<List<Integer>> data = new ArrayList<>();
-        data.add(Arrays.asList(115, 66));  // Female
-        data.add(Arrays.asList(175, 78));  // Male
-        data.add(Arrays.asList(205, 72));  // Male
-        data.add(Arrays.asList(120, 67));  // Female
-        data.add(Arrays.asList(120, 52));  // Female
-        data.add(Arrays.asList(185, 87));  // Male
-        data.add(Arrays.asList(199, 77));  // Male
-        data.add(Arrays.asList(105, 60));  // Female
-        data.add(Arrays.asList(150, 66));  // Female
-        data.add(Arrays.asList(160, 78));  // Male
-        data.add(Arrays.asList(189, 76));  // Male
-        data.add(Arrays.asList(142, 60));  // Female
-
-        List<Double> answers = Arrays.asList(1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0);
+    public void trainAndPredict(int numLayers, int epochs, int newHeight, int newWeight, List<List<Integer>> data, List<Double> lastColumn) {
 
         Network network500 = new Network(numLayers, epochs);
 
         // Train the network
-        network500.train(data, answers);
+        network500.train(data, lastColumn);
         double prediction = network500.predict(newHeight, newWeight);
         String result = (prediction <= 0.5) ? "Male" : "Female";
         // Display predictions
@@ -395,7 +150,9 @@ public class App2 {
                     predictions.add(this.predict(data.get(i).get(0), data.get(i).get(1)));
                 }
                 Double thisEpochLoss = Util.meanSquareLoss(answers, predictions);
-
+                if (epoch % 50 == 0)
+                    System.out.println(String.format("Epoch: %s | bestEpochLoss: %.15f | thisEpochLoss: %.15f", epoch,
+                            bestEpochLoss, thisEpochLoss));
                 if (bestEpochLoss == null || thisEpochLoss < bestEpochLoss) {
                     bestEpochLoss = thisEpochLoss;
                     epochNeuron.remember();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,38 +1,77 @@
+// import java.io.BufferedReader;
+// import java.io.FileInputStream;
+// import java.io.IOException;
+// import java.io.InputStreamReader;
+// import java.nio.charset.StandardCharsets;
+// import java.util.ArrayList;
+// import java.util.List;
 
+// public class Main {
+//     public static void main(String[] args) {
+//         List<Integer[]> data = new ArrayList<>();
+//         List<Integer> lastColumn = new ArrayList<>();
+
+//         try {
+//             FileInputStream fis = new FileInputStream("testdata.csv");
+//             InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+//             BufferedReader br = new BufferedReader(isr);
+
+//             String line;
+//             line = br.readLine();
+//             while ((line = br.readLine()) != null) {
+//                 String[] values = line.split(",");
+//                 // for(String v : values) System.out.println(v); // Debugging purposes only!
+//                 data.add(new Integer[] { Integer.valueOf(values[0]), Integer.valueOf(values[1]) });
+//                 lastColumn.add(Integer.parseInt(values[2]));
+//             }
+//         } catch (IOException e) {
+//             e.printStackTrace();
+//         }
+
+//         Integer[][] dataArray = new Integer[data.size()][2];
+//         for (int i = 0; i < data.size(); i++) {
+//             dataArray[i] = data.get(i);
+//         }
+
+//     }
+// }
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        List<String[]> data = new ArrayList<>();
-        List<Integer> lastColumn = new ArrayList<>();
+        List<List<Integer>> data = new ArrayList<>();
+        List<Double> lastColumn = new ArrayList<>();
 
-        try (BufferedReader br = new BufferedReader(new FileReader("testdata.csv"))) {
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(
+                new FileInputStream("testdata.csv"), StandardCharsets.UTF_8))) {
+
             String line;
-            while ((line = br.readLine())!= null) {
+            br.readLine(); // Skip the header line
+            while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                data.add(new String[]{values[0], values[1]});
-                lastColumn.add(Integer.parseInt(values[2]));
+                List<Integer> row = new ArrayList<>();
+                row.add(Integer.valueOf(values[0]));
+                row.add(Integer.valueOf(values[1]));
+                data.add(row);
+                lastColumn.add(Double.parseDouble(values[2]));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Convert the List to a 2D array
-        String[][] dataArray = new String[data.size()][2];
+        // Convert List<List<Integer>> to a 2D array (optional)
+        Integer[][] dataArray = new Integer[data.size()][2];
         for (int i = 0; i < data.size(); i++) {
-            dataArray[i] = data.get(i);
+            List<Integer> row = data.get(i);
+            dataArray[i] = row.toArray(new Integer[0]);
         }
 
-        // Print the arrays
-        for (String[] row : dataArray) {
-            System.out.println(Arrays.toString(row));
-        }
-        for (int value : lastColumn) {
-            System.out.println(value);
-        }
+        // Now you have 'data' as List<List<Integer>> and 'lastColumn' as List<Double>
     }
 }
